1adea22f913d541154e8e2c4a2ff56285d680e03
boat83 <66733033+boat83@users.noreply.github.com>
2021-09-06 15:03:15 +0200

    Add files via upload

--- testproject/test_k1.py
+++ testproject/test_k1.py
@@ -2,11 +2,11 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import InvalidSessionIdException
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
+
 URL = "https://ambitious-sky-0d3acbd03.azurestaticapps.net/k1.html"
 time.sleep(1)
 
